d33fe2e5b3cef2e3b2d75a668b0323d08871bb62
NSrikavya <navadu.srikavya@gmail.com>
2025-08-28 11:40:24 +0530

    commit

--- LAB4/A2.py
+++ LAB4/A2.py
@@ -4,29 +4,29 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_percentage_error
 
-# 1. Load dataset
+# Load dataset
 data = pd.read_excel(r"C:\Users\lenovo\OneDrive\Desktop\ML_Lab_Exercises\ML_Lab_Exercises\LAB4\Lab Session Data.xlsx")
 
-# 2. Keep only relevant numeric columns
+# Keep only relevant numeric columns
 data = data[['Candies (#)', 'Mangoes (Kg)', 'Milk Packets (#)', 'Payment (Rs)']]
 
-# 3. Features and Target
+# Features and Target
 X = data[['Candies (#)', 'Mangoes (Kg)', 'Milk Packets (#)']]
 y = data['Payment (Rs)']
 
-# 4. Train-Test Split
+# Train-Test Split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# 5. Train Model (Linear Regression)
+# Train Model (Linear Regression)
 model = LinearRegression()
 model.fit(X_train, y_train)
 
-# 6. Predictions
+# Predictions
 y_pred = model.predict(X_test)
 
-# 7. Metrics
+# Metrics
 mse = mean_squared_error(y_test, y_pred)
 rmse = np.sqrt(mse)
 mape = mean_absolute_percentage_error(y_test, y_pred)
