298b14d94f3eac6a7de13a9d1d0174a94fa64dc9
NSrikavya <navadu.srikavya@gmail.com>
2025-08-28 13:08:25 +0530

    commit

--- LAB4/A4.py
+++ LAB4/A4.py
@@ -2,25 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
 
-# 1. Generate 20 random training points 
+# Generate 20 random training points 
 np.random.seed(42)
 X_train = np.random.randint(1, 11, (20, 2))
 y_train = np.where(X_train[:, 0] + X_train[:, 1] < 10, 0, 1)
 
-# 2. Generate test data grid 
+# Generate test data grid 
 x_values = np.arange(0, 10.1, 0.1)
 y_values = np.arange(0, 10.1, 0.1)
 xx, yy = np.meshgrid(x_values, y_values)
 X_test = np.c_[xx.ravel(), yy.ravel()]  
 
-# 3. Train kNN classifier (k=3)
+# Train kNN classifier (k=3)
 knn = KNeighborsClassifier(n_neighbors=3)
 knn.fit(X_train, y_train)
 
-# 4. Predict test labels
+# Predict test labels
 y_pred = knn.predict(X_test)
 
-# 5. Scatter plot of test data 
+# Scatter plot of test data 
 plt.figure(figsize=(7, 7))
 plt.scatter(X_test[:, 0], X_test[:, 1], c=y_pred, cmap=plt.cm.bwr, alpha=0.3, s=10)
 
